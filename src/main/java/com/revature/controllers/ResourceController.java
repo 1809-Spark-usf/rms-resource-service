7b30289132d21bf63f9de78b23c7b03e8e1dfb2c
jkennedy5795 <jkennedy@email.sc.edu>
2018-12-04 15:17:11 -0500

    post added

--- src/main/java/com/revature/controllers/ResourceController.java
+++ src/main/java/com/revature/controllers/ResourceController.java
@@ -1,9 +1,12 @@
 package com.revature.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.models.Resource;
 import com.revature.services.ResourceService;
 
 @RestController
@@ -18,4 +21,8 @@ public class ResourceController {
 		this.resourceService = resourceService;
 	}
 	
+	@PostMapping("")
+	public Resource saveResource(@RequestBody Resource resource) {
+		return resourceService.save(resource);
+	}
 }
--- src/main/java/com/revature/services/ResourceService.java
+++ src/main/java/com/revature/services/ResourceService.java
@@ -3,6 +3,7 @@ package com.revature.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.revature.models.Resource;
 import com.revature.repository.ResourceRepository;
 
 @Service
@@ -16,4 +17,7 @@ public class ResourceService {
 		this.resourceRepo = resourceRepo;
 	}
 	
+	public Resource save(Resource resource) {
+		return resourceRepo.save(resource);
+	}
 }
