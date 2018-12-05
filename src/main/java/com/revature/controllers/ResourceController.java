9e0a5de5e89f13589145b1cea60e28cfd445b6cb
jbb111116 <jon.bed@hotmail.com>
2018-12-05 10:56:07 -0600

    Get method from Repo removed and added cast to the service

--- src/main/java/com/revature/controllers/ResourceController.java
+++ src/main/java/com/revature/controllers/ResourceController.java
@@ -3,10 +3,10 @@ package com.revature.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -63,6 +63,8 @@ public class ResourceController {
 	public List<Resource> getResources(Pageable pageable) {
 		return resourceService.getAllResources(pageable);
 	}
+	
+	
 	
 	/**
 	 * Gets a specific resource by Id.
