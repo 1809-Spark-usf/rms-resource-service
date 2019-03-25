8d3b9ab8f0529276c3f132ba3ac15e792a722ecb
NoahThacker31 <31545990+NoahThacker31@users.noreply.github.com>
2019-03-25 11:37:27 -0400

    Swagger UI

--- src/main/java/com/revature/Application.java
+++ src/main/java/com/revature/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
  * The Class Application.
  * Boot strap application that deals 
@@ -19,6 +21,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author 1811-Java-Nick | 12/27/2018
  * 
  */
+@EnableSwagger2
 @SpringBootApplication
 @EnableDiscoveryClient
 public class Application {
